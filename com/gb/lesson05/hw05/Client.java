015337201339ddc85e03ea7337510c66d4f48842
vl <chertonovskiy@icloud.com>
2024-01-01 08:45:03 +0700

    lesson 05

---
+++ com/gb/lesson05/hw05/Client.java
@@ -0,0 +1,54 @@
+package com.gb.lesson05.hw05;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.util.Objects;
+import java.util.Scanner;
+
+public class Client {
+    private static final int SERVER_PORT = Server.PORT;
+    private static final String URL = "localhost";
+
+    public static void main(String[] args){
+
+        try(Socket client = new Socket(URL, SERVER_PORT)){
+
+            new Thread(() -> {outputMessage(client);}).start();
+
+            InputMessage(client);
+
+        }catch (IOException e){
+            throw new RuntimeException(e);
+        }
+
+    }
+
+    private static void InputMessage(Socket client){
+        try (Scanner input = new Scanner(client.getInputStream())) {
+            while (!client.isClosed()) {
+                if(input.hasNext()){
+                    System.out.println(input.nextLine());
+                }
+            }
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private static void outputMessage(Socket client){
+        try (PrintWriter output = new PrintWriter(client.getOutputStream(), true)) {
+            Scanner consoleScanner = new Scanner(System.in);
+            while (true) {
+                String consoleInput = consoleScanner.nextLine();
+                output.println(consoleInput);
+                if (Objects.equals("q", consoleInput)) {
+                    client.close();
+                    break;
+                }
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
---
+++ com/gb/lesson05/hw05/SocketWrapper.java
@@ -0,0 +1,41 @@
+package com.gb.lesson05.hw05;
+
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.util.Scanner;
+
+
+public class SocketWrapper implements AutoCloseable {
+    private final long id;
+    private final Socket socket;
+    private final Scanner input;
+    private final PrintWriter output;
+
+    SocketWrapper(long id, Socket socket) throws IOException {
+        this.id = id;
+        this.socket = socket;
+        this.input = new Scanner(socket.getInputStream());
+        this.output = new PrintWriter(socket.getOutputStream(), true);
+    }
+
+
+    public Scanner getInput() {
+        return input;
+    }
+
+    public PrintWriter getOutput() {
+        return output;
+    }
+
+    @Override
+    public void close() throws Exception {
+        socket.close();
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%s", socket.getInetAddress().toString());
+    }
+}
